cd4a970b34e40449905872946ab41af651a40772
nafewprodhan <nafew.prodhan@gmail.com>
2021-10-27 17:19:39 +0600

    Serup footer & fixed login design and create login functionality

--- user/views.py
+++ user/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.dispatch.dispatcher import receiver
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.urls import conf
+from django.db.models import Q
 
 # Create your views here.
 
 def home(request):
     return render(request, 'main.html')
+
+def loginUser(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.success(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+            # return redirect(request.GET['next'] if 'next' in request.GET else 'account')
+
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+
+    return render(request, 'user/login_register.html')
